3b8af763fdc6ab08cabbc5f4849810e1ba0a9bbc
Shaukatjassme <shaukat.jassme786@gmsil.com>
2022-12-06 15:05:30 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Navbar } from './Components/Navbar';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+   
+    <Navbar/>
+  );
+}
+
+export default App;
